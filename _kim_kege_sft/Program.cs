aafe5ca665e3a69b80f13e5240040f58e39122cd
Sosov Nikolay <djwirtuoz@gmail.com>
2023-02-25 10:05:10 +0300

    Добавьте файлы проекта.

---
+++ _kim_kege_sft/Program.cs
@@ -0,0 +1,21 @@
+﻿string fileInputName = @"school1_list_program.xml";
+
+Console.WriteLine("Положите исходный .xml файл в папку с этой программой");
+Console.Write("Введите имя файла с расширением. По умолчанию будет использованно имя 'school1_list_program.xml' : ");
+
+fileInputName = Console.ReadLine();
+
+if (fileInputName == null || fileInputName == "")
+{
+    fileInputName = @"school1_list_program.xml";
+}
+
+try
+{
+    Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes(fileInputName)));
+    Console.WriteLine("Скопируйте содержимое окна в тестовый файл с расширением .sft");
+}
+catch
+{
+    Console.WriteLine("Файл с таким именем не найден.");
+}
